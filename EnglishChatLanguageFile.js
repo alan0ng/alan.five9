8698cb98173baca5ba6571c0615ff791d5597019
alan0ng <alan0ng.integrations@gmail.com>
2025-03-25 12:59:07 -0700

    Update EnglishChatLanguageFile.js

--- EnglishChatLanguageFile.js
+++ EnglishChatLanguageFile.js
@@ -32,7 +32,7 @@ chatLangCallback({
     "S_VIDEO_ACTIVATED": "Video and/or audio activated",
     "S_VIDEO_TERMINATED": "Video and/or audio terminated",
     "S_INPUT_MESSAGE_PLACEHOLDER": "Type here...",
-    "S_PRINT_TRANSCRIPT": "Printing Transcript",
+	"S_PRINT_TRANSCRIPT": "Printing Transcript",
     "S_REQUEST_TRANSCRIPT": "Emailed Transcript..."
   },
   "S_ERROR_PAGE": {
